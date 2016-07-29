b80817e63fa5d21038a38b2519bf7d86afe57ac1
Eduardo <edugonch@gmail.com>
2016-07-28 21:22:43 -0600

    Make the module loaded dynamic (I forget to remove the hard coded path)

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ module ContentfulApplication
       self.configure(File.join(File.dirname(__FILE__), 'config.yml'))
       Dir[File.join(File.dirname(__FILE__), '/initializers/*_initializer.rb')].each do |file| 
           require file
-          module_name = File.basename("/home/edugonch/Documents/contentful/initializers/email_initializer.rb", ".rb")
+          module_name = File.basename(file, ".rb")
           extend "ContentfulApplication::#{module_name.classify}".safe_constantize
       end
     end
